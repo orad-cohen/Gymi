d5675667089e997982b23918c40f5e5190114f78
Xccz0299@myoffice365.site <Xccz0299@myoffice365.site>
2021-12-02 10:52:38 +0200

    Added Meals button as a placeholder for the meals menu

--- app/src/main/java/com/project/gymi/Meal.java
+++ app/src/main/java/com/project/gymi/Meal.java
@@ -1,2 +1,31 @@
-package com.project.gymi;public class Meal {
+package com.project.gymi;
+
+public class Meal {
+
+    String name;
+    Double kCal;
+
+    public Meal() {
+    }
+
+    public Meal(String name, Double kCal) {
+        this.name = name;
+        this.kCal = kCal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getkCal() {
+        return kCal;
+    }
+
+    public void setkCal(Double kCal) {
+        this.kCal = kCal;
+    }
 }
